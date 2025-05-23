57e799d5d0b3ceebf8fa31571d0db3372dd96e3d
Manuelle <viktormanu64@gmail.com>
2025-05-23 14:19:21 -0500

    Avance

---
+++ app/src/main/java/com/example/w/login.java
@@ -0,0 +1,77 @@
+package com.example.w;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class login extends AppCompatActivity {
+
+    Button btnlogin;
+    EditText email, password;
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_login);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        mAuth = FirebaseAuth.getInstance();
+
+        email = findViewById(R.id.txtemail);
+        password = findViewById(R.id.txtpassword);
+        btnlogin = findViewById(R.id.btnlogin);
+
+        btnlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String emailUser = email.getText().toString().trim();
+                String passwordUser = password.getText().toString().trim();
+
+                if(emailUser.isEmpty() && passwordUser.isEmpty()){
+                    Toast.makeText(login.this, "Llene todos los campos", Toast.LENGTH_SHORT).show();
+                }else{
+                    loginUser(emailUser, passwordUser);
+                }
+            }
+        });
+    }
+    private void loginUser(String emailUser, String passwordUser){
+        mAuth.signInWithEmailAndPassword(emailUser, passwordUser).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if (task.isSuccessful()){
+                    finish();
+                    startActivity(new Intent(login.this, MainActivity.class));
+                }else{
+                    Toast.makeText(login.this, "Error", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(login.this, "Error al iniciar sesión", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
